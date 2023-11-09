2ac8588edad61a0e7b5394514da44deadd8038f4
MichalskiLucas <lucasmichalski.pessoal@gmail.com>
2023-11-08 21:56:48 -0300

    Feat: Criado procedimento para envio de email

--- CookMaster-APP/cookmasterapi/src/main/java/br/integration/cookmasterapi/services/IngredienteService.java
+++ CookMaster-APP/cookmasterapi/src/main/java/br/integration/cookmasterapi/services/IngredienteService.java
@@ -3,6 +3,7 @@ package br.integration.cookmasterapi.services;
 import java.util.*;
 
 import br.integration.cookmasterapi.enums.EnumUnitMeasure;
+import br.integration.cookmasterapi.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,54 +18,55 @@ public class IngredienteService {
 
 	@Autowired
 	private EmailService emailService;
-	
+
+	@Autowired
+	private UsuarioService usuarioService;
 	public Ingrediente insert(Ingrediente ingrediente) throws Exception {
 
 		validaInsert(ingrediente);
 		Ingrediente i = ingredienteRepository.saveAndFlush(ingrediente);
-		emailService.sendMail("kedssy533779@gmail.com", "ingrediente Criado", "Ingrediente com id: " + i.getId() +" criado");
-
+		sendEmailForAdmin(i);
 		return i;
-		
+
 	}
-	
+
 	public Ingrediente edit(Ingrediente ingrediente) throws Exception {
 		//validarEdicao(ingrediente);
 		ingredienteRepository.saveAndFlush(ingrediente);
 		return ingrediente;
-		
+
 	}
-	
-	public List<Ingrediente> findAll(){
+
+	public List<Ingrediente> findAll() {
 		return ingredienteRepository.findAll();
 	}
-	
-	public Ingrediente findById(Long id) throws Exception{
-		Optional<Ingrediente> retorno =  ingredienteRepository.findById(id);
-		if(retorno.isPresent())
+
+	public Ingrediente findById(Long id) throws Exception {
+		Optional<Ingrediente> retorno = ingredienteRepository.findById(id);
+		if (retorno.isPresent())
 			return retorno.get();
 		else
-			throw new Exception("Ingrediente com ID: " + id+" não identificado!");
+			throw new Exception("Ingrediente com ID: " + id + " não identificado!");
 	}
-	
+
 	public List<Ingrediente> findByFilters(String descricao) {
 		return ingredienteRepository.findByDescricaoContainingAllIgnoringCase(descricao);
 	}
-	
+
 	public Ingrediente findByDescricao(String descricao) {
 		return ingredienteRepository.findIngredienteByDescricao(descricao);
 	}
-	
-	public void validaInsert(Ingrediente ingrediente) throws Exception{
-        if (ingrediente.getId() != null){
-            throw new Exception("Não deve informar o ID para inserir o ingrediente");
-        }
-        if(findByDescricao(ingrediente.getDescricao()) != null){
-        	throw new Exception("Ingrediente com a mesma descrição já inserido");
-        }
-    }
 
-	public List<Map<String, String>> getUnitMeasure(){
+	public void validaInsert(Ingrediente ingrediente) throws Exception {
+		if (ingrediente.getId() != null) {
+			throw new Exception("Não deve informar o ID para inserir o ingrediente");
+		}
+		if (findByDescricao(ingrediente.getDescricao()) != null) {
+			throw new Exception("Ingrediente com a mesma descrição já inserido");
+		}
+	}
+
+	public List<Map<String, String>> getUnitMeasure() {
 		List<Map<String, String>> result = new ArrayList<>();
 		for (EnumUnitMeasure e : EnumUnitMeasure.values()) {
 			Map<String, String> m = new HashMap<>();
@@ -73,5 +75,16 @@ public class IngredienteService {
 			result.add(m);
 		}
 		return result;
+	}
+
+	public void sendEmailForAdmin(Ingrediente r) throws Exception {
+		String corpo = "Ingrediente com id: " + r.getId() + " encontra-se pendente de aprovação.";
+		String assunto = "Ingrediente pendente de aprovação";
+		List<Usuario> usuarios = usuarioService.findAdmin();
+
+		for (int i = 0; i < usuarios.size(); i++) {
+			emailService.sendMail(usuarios.get(i).getEmail(), corpo, assunto);
+		}
+
 	}
 }
